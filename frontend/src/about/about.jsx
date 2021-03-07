25968397ebb538d572f2cd20d4f62e1422c54eb3
Thonny Noda <noda.thonny@gmail.com>
2021-03-07 11:53:59 -0300

    Criando o Repositorio todo-app

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nos'></PageHeader>
+
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut hic voluptatibus accusamus, pariatur recusandae architecto quae quasi dolorum? Cumque odio tempora, recusandae animi et libero maiores modi expedita vero eius!</p>
+        
+        <h2>Missao e Visao</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut hic voluptatibus accusamus, pariatur recusandae architecto quae quasi dolorum? Cumque odio tempora, recusandae animi et libero maiores modi expedita vero eius!</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut hic voluptatibus accusamus, pariatur recusandae architecto quae quasi dolorum? Cumque odio tempora, recusandae animi et libero maiores modi expedita vero eius!</p>
+
+    </div>
+)
